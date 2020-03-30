2eee83fb8a90bb2d0a944f442322e497ef5c95f3
scammi <scammi@gmail.com>
2020-03-30 17:11:28 -0300

    curl test

--- responce.php
+++ responce.php
@@ -1,9 +1,14 @@
 <?php
-$responce =  file_get_contents("https://npiregistry.cms.hhs.gov/api/?number=".$_GET["npi"]."&version=2.1");
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-$json = json_decode($responce);
-// $orgName = $json["results"][0]["number"];
-// print_r($orgName);
+$ch = curl_init();
 
-print_r($json);
+curl_setopt($ch, CURLOPT_URL, "https://npiregistry.cms.hhs.gov/api/?number=".$_GET["npi"]."&version=2.1");
+$result = curl_exec($ch);
+print_r($result);
+
+curl_close($ch);
+
 ?>
